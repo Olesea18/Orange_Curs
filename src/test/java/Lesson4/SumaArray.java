a575d0ffffea3fc6c16e9f09a57b8a2964077fb6
OGAMURARI <olesea.gamurari@orange.com>
2024-04-23 18:07:39 +0300

    Third Commit

--- src/test/java/Lesson4/SumaArray.java
+++ src/test/java/Lesson4/SumaArray.java
@@ -13,7 +13,32 @@ public class SumaArray {
         }
         System.out.println("Suma"+" "+sum);
 
+        int [] array = {1,2,3,4,5,6};
+        for(int i=0; i< array.length;i++){
+            System.out.println(array[i]);
+        }
 
+        int [] first = {5,5,4,3,1};
+        int [] second = {3,5,8,10,1};
+
+        int max = first[0];
+        for (int i=1; i< first.length; i++){
+
+            max = Math.max(max, first[i]);
+        }
+        System.out.println(max);
+
+        int min = second[0];
+        for (int i=1; i< second.length; i++){
+
+            min = Math.min(min,second[i]);
+        }
+        System.out.println(min);
+
+        String [] programingLanguages={"Java", "C++", "Pascal"};
+        for (String i: programingLanguages){
+            System.out.println(i);
+        }
     }
 
 }
